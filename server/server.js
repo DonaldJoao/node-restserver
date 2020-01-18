a73981027499f7b8b40308702d7504e7f6ef453f
Donald Joao <dedojo-2728@outlook.com>
2020-01-18 16:34:56 -0700

    Rest Server - usuario

--- server/server.js
+++ server/server.js
@@ -1,40 +1,18 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', (req, res) => {
-    res.json("get Usuario")
-});
-
-app.post('/usuario', (req, res) => {
-    let body = req.body;
-    if(body.nombre === undefined){
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }
-    else{
-        res.json({
-            persona: body
-        })
-    }
-});
-
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', (req, res) => {
-    res.json("delte Usuario")
+mongoose.connect(process.env.URLDB,
+                {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}, (err, res) => {
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () =>{
